2d1b5aa0379dd2119887cafb1ce7c3f3771c47c1
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-14 00:59:28 -0300

    add audios

--- utils/horizonUtils.js
+++ utils/horizonUtils.js
@@ -1,6 +1,6 @@
 let config;
 try {
-    config = require('../config.json');
+    config = require("../config.json");
 } catch (error) {
     config = null;
 }
@@ -10,3 +10,7 @@ exports.mongoUri = config ? config.mongoUri : process.env.MONGO_URI;
 exports.guildId = config ? config.guild : process.env.GUILD;
 exports.channel = config ? config.channel : process.env.CHANNEL;
 exports.avatar = config ? config.avatar : process.env.AVATAR;
+exports.audios = {
+    tuturu: config ? config.audio.tuturu : process.env.TUTURU,
+    mvpAlert: config ? config.audio.mvpAlert : process.env.MVP_ALERT,
+};
